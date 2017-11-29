929ed27369441f50fdd3e68eb18eecec0d7a63c3
Nicolas Kang <121809625@qq.com>
2017-11-29 15:46:18 -0800

    first

---
+++ dwt2d/run_gpu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -N BNCHMRK_DWT2D
+#$ -q gpu
+#$ -l gpu=1
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64"
+export CUDA_HOME=/usr/local/cuda
+module load cuda/8.0
+module load gcc/4.4.3
+
+echo "Script began:" `date`
+echo "Node:" `hostname`
+echo ""
+echo "Current directory:${PWD}"
+echo ""
+echo "Current GPU:$(nvidia-smi)"
+echo ""
+echo ""
+echo "running dwt2d"
+for trial in 1 2; do
+echo "*** Trial ${trial} ***"
+./run
+echo "Current GPU:$(nvidia-smi)"
+done
+
+echo ""
+echo "=== done! ==="
+~                     
