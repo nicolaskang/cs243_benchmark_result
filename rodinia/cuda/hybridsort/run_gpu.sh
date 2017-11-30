eec1aaa0f425f299e6542cc0544b0b8fbb128d46
Nicolas Kang <121809625@qq.com>
2017-11-29 16:55:16 -0800

    big change

---
+++ rodinia/cuda/hybridsort/run_gpu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#$ -N BNCHMRK_HYBRIDSORT
+#$ -q gpu2
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64"
+export CUDA_HOME=/usr/local/cuda
+module load cuda/8.0
+module load gcc/4.4.3
+
+echo "Script began:" `date`
+echo "Node:" `hostname`
+echo ""
+echo "Current directory:${PWD}"
+echo ""
+echo "Current GPU:$(nvidia-smi)"
+echo ""
+echo ""
+echo "running b+tree"
+for trial in 1 ; do
+echo "*** Trial ${trial} ***"
+./run
+done
+
+echo ""
+echo "=== done! ==="
+
---
+++ rodinia/cuda/kmeans/compile.sh
@@ -0,0 +1,4 @@
+module load cuda/8.0
+module load gcc/4.4.3
+make clean
+make
